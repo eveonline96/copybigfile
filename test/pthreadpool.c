2a58bc251c3bbc58b669fb431cfed455ae134282
eveonline96 <870402433@qq.com>
2017-08-02 23:51:33 +0800

    modify_test

--- test/pthreadpool.c
+++ test/pthreadpool.c
@@ -33,6 +33,111 @@ typedef struct pthreadpool
 	int tasktotalcnt; //	任务总任务数
 }pthreadpool;
 
+
+//初始化队列
+void init_queue(taskqueue*qtask)
+{
+	qtask->head=NULL;
+	qtask->tail=NULL;
+}
+//进队操作
+void push_queue(taskqueue *qtask,fileblock*fblock)
+{
+	tasknode *new_tnode=(tasknode *)malloc(1*sizeof(tasknode));
+	new_tnode->fpblock=fblock;
+	new_tnode->next=NULL;
+	if (qtask->head==NULL && qtask->tail==NULL)
+	{
+		qtask->head=new_tnode;
+		qtask->tail=new_tnode;
+	}
+	else
+	{
+		qtask->tail->next=new_tnode;
+		qtask->tail=new_tnode;
+	}
+}
+
+//创建任务队列
+void init_task_list(unsigned int fd)
+{
+	g_taskqueuep=(taskqueue*)malloc(1*sizeof(taskqueue));
+	if (g_taskqueuep==NULL)
+	{
+		ERR_EXIT("init task list fail");
+	}
+	init_queue(g_taskqueuep);
+	g_blockfp=file_block(fd);
+	int i;
+	for(i=0;i<g_pool->tasktotalcnt;i++)
+	{
+		push_queue(g_taskqueuep,&g_blockfp[i]);
+		g_pool->taskcnt++;
+		cout<<"taskcnt="<<g_pool->taskcnt<<endl;   //记录
+		pthread_cond_signal(g_pool->cond);  //唤醒
+	}
+}
+
+
+//判断队列是否为空
+int is_empty_queue(taskqueue *qtask)
+{
+	int isempty=1;
+	if (qtask->head==NULL && qtask->tail==NULL)
+	{
+		isempty=0;
+	}
+	return isempty;
+}
+//出队列操作
+tasknode * pop_queue(taskqueue * qtask)
+{
+	tasknode * returnnode=NULL;
+	if (is_empty_queue(qtask))
+	{
+		returnnode=qtask->head;
+		qtask->head=qtask->head->next;
+		if (qtask->head==NULL)
+		{
+			qtask->tail=NULL;
+		}
+		returnnode->next=NULL;
+	}
+	return returnnode;
+}
+//释放队列
+void free_queue(taskqueue *qtask)
+{
+	if (qtask!=NULL&&qtask->head==NULL &&qtask->tail==NULL)
+	{
+		free(qtask);
+		qtask=NULL;
+	}
+}
+
+
+void *Run(void *arg)
+{
+	while(1)
+	{
+		pthread_mutex_lock(g_pool->mutex);
+		g_pthreadcnt++;
+		if (g_pool->taskcnt==0&& g_gool->isshutdown==0)
+		{
+			pthread_cond_wait(g_pool->cond,g_pool->mutex);
+		}
+		g_pool->head=pop_queue(g_taskqueuep);
+		pthread_mutex_unlock(g_pool->mutex);
+		if (g_pool->head!=NULL)
+		{
+			g_pool->head=NULL;
+			g_pool->taskcnt--;
+			printf("%d\n",g_gool->tasktotal);
+				
+		}
+	}
+}
+
 void init_pthread_pool(int tasktotalcnt)
 {
 	g_pool=(pthreadpool *)malloc(1*sizeof(pthreadpool));
--- test/test1.cpp
+++ test/test1.cpp
@@ -1,42 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/mman.h>
-#include <iostream>
-#include <cstring>
-#include <string>
 using namespace std;
 
-
+int floor(int n)
+{
+	if (n==1)
+	{
+		return 1;
+	}
+	if (n==2)
+	{
+		return 2;
+	}
+	if (n==3)
+	{
+		return 3;
+	}
+	if (n>3)
+	{
+		return floor(n-1)+floor(n-2)+floor(n-3);
+	}
+}
 int main(int argc, char const *argv[])
 {
-	//char *ch1=(char*)malloc(100*sizeof(char));
-	// char ch1[100];
-	// char *ch2="new";
-	// char ch3[100];
-	// scanf("%s",&ch1);
-	// strcpy(ch3,ch2);
-	// strcat(ch3,ch1);
-	// printf("%s\n",ch3);
-
-	char ch1[100];
-	char ch2[10]="new_";
-	scanf("%s",&ch1);
-	//strcat(ch2,ch2);
-	open(ch1,O_CREAT|O_EXCL|O_RDWR,0777);
-	printf("open ok\n");
-	//printf("%s\n",ch2);
-
-	// string str1=" ";
-	// cin>>str1>>endl;
-	// string str2="new";
-	// string str3=str2+str1;
-	// cout<<str3<<endl;
+	int n=15;
+	int a=floor(n);
+	printf("%d\n",a );
 	return 0;
 }
